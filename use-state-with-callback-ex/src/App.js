6fc201cb87ea7a5319b5aeb049be97808782c214
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2021-10-18 23:03:01 +0530

    useStateWithCallbackLazy instead useState

--- use-state-with-callback-ex/src/App.js
+++ use-state-with-callback-ex/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useStateWithCallbackLazy} from 'use-state-with-callback';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useStateWithCallbackLazy(0);
+
+  const handleClick=()=>{
+    setCount(count + 1,(latestCount)=>{
+      console.log("Hello",latestCount);
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <butto onClick={handleClick}>Click</butto>
     </div>
-  );
+  )
 }
-
-export default App;
